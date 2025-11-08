65e6438a9805e647f4a34e7d09d7e15fe95c5ce4
estebansar <sar17008@byui.edu>
2025-11-07 21:01:56 -0700

    get all data and return it to the app.js

--- js/data.mjs
+++ js/data.mjs
@@ -1,2 +1,10 @@
 const SCRIPTURES_JSON = "./data/scriptures.json";
 const QUOTES_JSON = "./data/quotes.json";
+
+export async function fetchAll() {   //get all data and return it to app.js
+    const s = await fetch(SCRIPTURES);
+    const q = await fetch(QUOTES);
+    const scriptures = await s.json();
+    const quotes = await q.json()
+    return { scriptures: scriptures, quotes: quotes };
+}
